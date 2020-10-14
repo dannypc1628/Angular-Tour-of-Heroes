a6fc304bf98a7ffc2d6d4a7fb4ad7e449a1a0b08
dannyLiu <h152074716@gmail.com>
2020-10-14 17:27:11 +0800

    為hero-detail建立路由，並抽離與heroComponent的主從關係

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,17 +11,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`使用者選擇${hero.id}號 ${hero.name}英雄`);
   }
 
   getHeroes(): void {
